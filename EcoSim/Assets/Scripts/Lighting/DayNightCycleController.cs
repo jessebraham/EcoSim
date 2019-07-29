2f84426706aedc00e815f80cc559f3042740e4bb
Jesse Braham <jesse@beta7.io>
2019-07-29 14:46:54 -0700

    Add sun/moon, implement simple day/night cycle

---
+++ EcoSim/Assets/Scripts/Lighting/DayNightCycleController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class DayNightCycleController : MonoBehaviour
+{
+    public bool Active         = true;
+    public float SecondsPerDay = 30f;
+
+
+    void Update()
+    {
+        if (Active)
+        {
+            transform.RotateAround(Vector3.zero, Vector3.right, 360f / SecondsPerDay * Time.deltaTime);
+            transform.LookAt(Vector3.zero);
+        }
+    }
+}
