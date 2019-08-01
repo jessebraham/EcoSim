9ca306ecf8efc56462668ec207d8531960adbf3b
Jesse Braham <jesse@beta7.io>
2019-07-31 21:16:48 -0700

    Implement a global World Clock

--- EcoSim/Assets/Scripts/Lighting/DayNightCycleController.cs
+++ EcoSim/Assets/Scripts/Lighting/DayNightCycleController.cs
@@ -2,54 +2,23 @@
 
 public class DayNightCycleController : MonoBehaviour
 {
-    // ------------------------------------------------------------------------
-    // PUBLIC
-
-    public bool  active        = true;
-    public float secondsPerDay = 30f;
-
-
-    // ------------------------------------------------------------------------
-    // PRIVATE
-
-    private Light sun;
-
-    private float timer = 0f;
-
-    private float PercentComplete
-    {
-        get
-        {
-            return timer / secondsPerDay;
-        }
-    }
-    private bool IsNight
-    {
-        get
-        {
-            return PercentComplete > 0.5f;
-        }
-    }
+    private WorldClock clock;
+    private Light      sun;
 
 
     public void Awake()
     {
-        sun = GameObject.Find("/Sun").GetComponent<Light>();
+        clock = GameObject.Find("/Clock").GetComponent<WorldClock>();
+        sun   = GameObject.Find("/Sun").GetComponent<Light>();
     }
 
     public void Update()
     {
-        if (active)
+        if (clock.active)
         {
-            timer += Time.deltaTime;
-            if (timer > secondsPerDay)
-            {
-                timer = 0f;
-            }
-
             UpdateSunIntensity();
 
-            transform.RotateAround(Vector3.zero, Vector3.right, 360f / secondsPerDay * Time.deltaTime);
+            transform.RotateAround(Vector3.zero, Vector3.right, 360f / clock.secondsPerDay * Time.deltaTime);
             transform.LookAt(Vector3.zero);
         }
     }
@@ -57,7 +26,7 @@ public class DayNightCycleController : MonoBehaviour
 
     private void UpdateSunIntensity()
     {
-        if (IsNight)
+        if (clock.IsNight)
         {
             if (sun.intensity > 0f)
             {
---
+++ EcoSim/Assets/Scripts/WorldClock.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class WorldClock : MonoBehaviour
+{
+    // ------------------------------------------------------------------------
+    // PUBLIC
+
+    public bool  active        = true;
+    public float secondsPerDay = 30f;
+
+    public int PercentComplete
+    {
+        get
+        {
+            return Mathf.RoundToInt(timer / secondsPerDay * 100);
+        }
+    }
+    public bool IsNight
+    {
+        get
+        {
+            return PercentComplete > 50;
+        }
+    }
+
+
+    // ------------------------------------------------------------------------
+    // PRIVATE
+
+    private float timer = 0f;
+
+
+    public void Update()
+    {
+        if (active)
+        {
+            timer += Time.deltaTime;
+            if (timer > secondsPerDay)
+            {
+                timer = 0f;
+            }
+        }
+    }
+}
