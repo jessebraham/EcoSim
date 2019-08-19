3ff22564e057cbefa9299df8467886cc6a17388d
Jesse Braham <jesse@beta7.io>
2019-08-18 20:44:30 -0700

    Add a property to the World Clock to compute the "time of day" in game

--- EcoSim/Assets/Scripts/WorldClock.cs
+++ EcoSim/Assets/Scripts/WorldClock.cs
@@ -5,21 +5,41 @@ public class WorldClock : MonoBehaviour
     // ------------------------------------------------------------------------
     // PUBLIC
 
-    public bool  active        = true;
-    public float secondsPerDay = 30f;
+    public bool  active         = true;
+    public float secondsPerDay  = 30f;
+    public float dayLengthHours = 24f;
 
-    public int PercentComplete
+    public float PercentComplete
     {
         get
         {
-            return Mathf.RoundToInt(timer / secondsPerDay * 100);
+            return timer / secondsPerDay;
         }
     }
     public bool IsNight
     {
         get
         {
-            return PercentComplete > 50;
+            return PercentComplete > 0.5f;
+        }
+    }
+    public System.TimeSpan WorldTime
+    {
+        get
+        {
+            float progress = dayLengthHours * PercentComplete;
+
+            int hour = (int)progress;
+            if (hour > 23) hour = 0;
+
+            float fMinute = (progress - hour) * 60f;
+            int   minute  = (int)fMinute;
+            if (minute > 59) minute = 0;
+
+            int second = (int)((fMinute - minute) * 60f);
+            if (second > 59) second = 0;
+
+            return new System.TimeSpan(hour, minute, second);
         }
     }
 
